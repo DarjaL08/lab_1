35e140f4ae69457b1d57c3d04cc0f8a1299e2f8c
DarjaL08 <Darja.Loktionova@edu.rtu.lv>
2025-02-12 16:17:47 +0000

    Lab1

--- LL_2.py
+++ LL_2.py
@@ -22,7 +22,18 @@ class LinkedList:
         return True
 
     # WRITE HAS_LOOP METHOD HERE #
-    #                            #
+    def has_loop(self):
+        slow = self.head
+        fast = self.head
+
+        while fast and fast.next:
+             slow = slow.next
+             fast=fast.next.next
+
+             if slow==fast:   
+                return True
+             
+        return False                    
     #                            #
     #                            #
     #                            #
@@ -35,6 +46,7 @@ my_linked_list_1 = LinkedList(1)
 my_linked_list_1.append(2)
 my_linked_list_1.append(3)
 my_linked_list_1.append(4)
+my_linked_list_1.append(4)
 my_linked_list_1.tail.next = my_linked_list_1.head
 print(my_linked_list_1.has_loop() ) # Returns True
 
@@ -44,6 +56,7 @@ print(my_linked_list_1.has_loop() ) # Returns True
 my_linked_list_2 = LinkedList(1)
 my_linked_list_2.append(2)
 my_linked_list_2.append(3)
+my_linked_list_2.append(4)
 my_linked_list_2.append(4)
 print(my_linked_list_2.has_loop() ) # Returns False
 
